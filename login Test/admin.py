41cd81f89225752d5d022c33d0703140067d16a0
Hritik Dey <110844175+hritik-dey2002@users.noreply.github.com>
2024-08-31 23:32:49 +0530

    Update admin.py

--- login Test/admin.py
+++ login Test/admin.py
@@ -3,6 +3,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import spacy
 spacy.load('en_core_web_sm')
+import nltk
+nltk.download('stopwords')
 
 import pandas as pd
 import time, datetime
